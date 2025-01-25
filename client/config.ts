d5c9fd707a25d6f5a59b0e19b3b1d39187150964
Massive12356 <edwardmintah17@gmail.com>
2025-01-25 16:19:33 +0000

    added BaseUrl

--- client/config.ts
+++ client/config.ts
@@ -6,7 +6,7 @@ const checkConfig = (server: string): Config | {} => {
   switch (server) {
     case "production":
       config = {
-        baseUrl: "",
+        baseUrl: "https://e-com-plus.vercel.app",
       };
       break;
     case "local":
@@ -20,5 +20,5 @@ const checkConfig = (server: string): Config | {} => {
   return config;
 };
 
-export const selectServer = "production";
+export const selectServer = "";
 export const config = checkConfig(selectServer) as Config;
